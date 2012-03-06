53b98c675607f7c6221a3abbd1b692c046cf418a
PanPiotrek <piotrzuza@gmail.com>
2012-03-06 22:36:32 +0100

    up

---
+++ JavaMessenger/src/MySQLConnector/DataConnector.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MySQLConnector;
+import java.sql.Connection;
+import java.sql.*;
+/**
+ *
+ * @author Piotr
+ */
+public class DataConnector {
+    public static void main(String args[])
+    {
+            System.out.println("MySQL Connect Example.");
+            Connection conn = null;
+            String url = "jdbc:mysql://sql09.freemysql.net/";
+            String dbName = "jmcdata";
+            String driver = "com.mysql.jdbc.Driver";
+            String userName = "peross89"; 
+            String password = "pwrpiotr89";
+            try {
+                Class.forName(driver).newInstance();
+                String dbURL = url+ "project?user=" + userName + "&password=" + password;
+                //conn = DriverManager.getConnection(url+dbName,userName,password);
+                //conn = DriverManager.getConnection(url+dbName,userName,password);
+                conn = DriverManager.getConnection(dbURL);
+                System.out.println("Connected to the database");
+                conn.close();
+                System.out.println("Disconnected from database");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+    }
+    
+}
