1262ce293e7653881e60591882e73851cf722b3c
PanPiotrek <piotrzuza@gmail.com>
2012-03-08 18:21:39 +0100

    up

--- JavaMessenger/src/DBSupport/TrySelect.java
+++ JavaMessenger/src/DBSupport/TrySelect.java
@@ -22,25 +22,25 @@ public class TrySelect {
     public boolean selectAuthentication(String a, String b)
     {
         boolean islooged = false;
-        System.out.println(" selectAuthentication : "+ a + " , " + b);
+        System.out.println(" selectAuthentication ");
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("JavaMessengerPU");
         EntityManager em = emf.createEntityManager();
         
         em.getTransaction().begin();
-        System.out.println(" TRANSACTRION BEGIN");
-        
+        System.out.println(" Transakcja rozpoczęta" + " login "+ a + " pass " + b);
+        System.out.println(" islogged = "+islooged);
         Query query = em.createNamedQuery( "Users.findByUserName");
-        //query.setParameter("id", 1L);
-        query.setParameter(a, 1L);
+        query.setParameter("userName", a);
         List<Users> users = query.getResultList();
         for (Users u : users) {
             System.out.println (u.getId() + " " + u.getUserName() + " " + u.getPassword());
+            System.out.println("Tester ");
             if(u.getPassword().equals(b))
             {islooged = true;
+                System.out.println("Jest zalogowany "+islooged);
             }
-              
-         }
-        
+        }
+        System.out.println("Czy jest zalogowany? "+islooged);
         em.getTransaction().commit();
         em.close();
         emf.close();
