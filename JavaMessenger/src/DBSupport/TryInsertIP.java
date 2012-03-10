fff3ac20696b8472a899e2d9355b73fbf432a116
PanPiotrek <piotrzuza@gmail.com>
2012-03-10 19:43:05 +0100

    up

--- JavaMessenger/src/DBSupport/TryInsertIP.java
+++ JavaMessenger/src/DBSupport/TryInsertIP.java
@@ -42,13 +42,11 @@ public class TryInsertIP {
                 query.setParameter("userName", a);
                 List<Users> users = query.getResultList();
                 for (Users u : users) {
-                    System.out.println("setIPFLAG: " + setIpFlag);
                     if (setIpFlag) {
                         u.setIp("" + this.getMyPublicIP());
                         System.out.println("Moje IP publicze " + this.getMyPublicIP());
                     } else {
                         u.setIp(null);
-                        System.out.println("Wylogowany");
                     }
 
                     em.persist(u);
