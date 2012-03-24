8978fa7aaac6feadb488f357ebbf6fceb1b60bcb
PanPiotrek <piotrzuza@gmail.com>
2012-03-24 12:11:26 +0100

    create sslsocket for file and text message

---
+++ JavaMessenger/src/Temps/SSLsocketFileTransmission/SSLClient_1.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Temps;
+
+import Others.JMHelper;
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import javax.net.ssl.*;
+
+/**
+ *
+ * @author SysOp
+ */
+public class SSLClient {
+
+    private static int PORT = 5050;
+    private SSLSocketFactory factory = null;
+    private SSLSocket socket = null;
+    private DataInputStream console = null;
+    private DataOutputStream streamOut = null;
+    private String host;
+
+    public SSLClient() {
+        System.setProperty("javax.net.ssl.trustStore", "testKey");
+        System.setProperty("javax.net.ssl.trustStorePassword", "tester");
+    }
+
+    public void prepare() {
+        host = JMHelper.getMyPublicIP(); //dla polaczen zdalnych wpisz adres ip
+        System.out.println("Establishing connection. Please wait ...");
+        try {
+            factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+            socket = (SSLSocket) factory.createSocket(host, PORT);
+            System.out.println("Connected: " + socket);
+            console = new DataInputStream(System.in);
+            streamOut = new DataOutputStream(socket.getOutputStream());
+
+        } catch (UnknownHostException uhe) {
+            System.out.println("Host unknown: " + uhe.getMessage());
+        } catch (IOException ioe) {
+            System.out.println("Unexpected exception: " + ioe.getMessage());
+        }
+    }
+
+    public void run() {
+        String line = "";
+        while (!line.equals(".bye")) {
+            try {
+                line = console.readLine();
+                streamOut.writeUTF(line);
+                streamOut.flush();
+            } catch (IOException ioe) {
+                System.out.println("Sending error: " + ioe.getMessage());
+            }
+        }
+        close();
+    }
+
+    public void close() {
+        try {
+            if (console != null) {
+                console.close();
+            }
+            if (streamOut != null) {
+                streamOut.close();
+            }
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException ioe) {
+            System.out.println("Error closing ...");
+        }
+    }
+}
